06ee17643356877609b9aba168eec8ff90c0a52c
Lasovar <luaidev@pm.me>
2025-05-06 13:44:23 +0530

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("theprimeagen")
+
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+
+local vimrc = vim.fn.stdpath("config") .. "/.vimrc"
+vim.cmd("source " .. vimrc)
+
+-- hello fem
+
---
+++ lua/theprimeagen/lazy/clippy.lua
@@ -0,0 +1,4 @@
+  return { -- Clippy diagnostics
+    'johnsaigle/clippy.nvim',
+    dependencies = { "jose-elias-alvarez/null-ls.nvim" },
+  }
